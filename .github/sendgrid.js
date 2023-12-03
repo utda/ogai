67edc41b08e49bb4f9a7b17bcbe74e952345a46f
Satoru <you@example.com>
2023-12-03 17:53:02 +0900

    fix: bug

---
+++ .github/sendgrid.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const msg = {
+    to: 'na.kamura.1263@gmail.com',
+    from: 'sender@example.org',
+    subject: 'Hello world',
+    text: 'Hello plain world!',
+    html: '<p>Hello HTML world!</p>',
+};
+
+sgMail
+    .send(msg)
+    .then(() => console.log('Mail sent successfully'))
+    .catch(error => console.error(error.toString()));

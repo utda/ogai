73ba28fda4d7b40231e681d0f9c7e17c82b69f07
Satoru <you@example.com>
2023-12-09 06:55:20 +0900

    feat: update message

--- .github/sendgrid.js
+++ .github/sendgrid.js
@@ -6,11 +6,26 @@ sgMail.setApiKey(apiKey);
 
 const to = process.env.SENDGRID_TO.split(',');
 
+text = `こんにちは、
+
+デプロイが正常に完了しました。以下は詳細情報です：
+
+- リポジトリ名: ${ process.env.GITHUB_REPOSITORY }
+- ブランチ名: ${ process.env.GITHUB_BRANCH }
+- コミットハッシュ: ${ process.env.GITHUB_SHA }
+- デプロイ日時: ${ process.env.GITHUB_TIMESTAMP }
+
+新しい変更が本番環境に反映されました。不具合や問題がある場合は、速やかに対処いたします。
+
+ご質問や疑問点があれば、お気軽にお知らせください。
+
+ありがとうございました。`
+
 const msg = {
     to,
     from: 'na.kamura.1263@gmail.com',
-    subject: '[utda/ogai] デプロイが完了しました',
-    text: '[utda/ogai] デプロイが完了しました',
+    subject: `デプロイが完了しました - ${ process.env.GITHUB_REPOSITORY }`,
+    text,
 };
 
 sgMail

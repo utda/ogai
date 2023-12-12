c526f1fca5cadd39ed01e1ebeadbb244625d08b5
Satoru <you@example.com>
2023-12-12 16:12:22 +0900

    feat: add sic and corr

--- components/Tei/Choice.vue
+++ components/Tei/Choice.vue
@@ -10,6 +10,9 @@ const props = withDefaults(defineProps<PropType>(), {
 const orig = props.node ? props.node.querySelector("tei-orig") : null;
 const reg = props.node ? props.node.querySelector("tei-reg") : null;
 
+const sic = props.node ? props.node.querySelector("tei-sic") : null;
+const corr = props.node ? props.node.querySelector("tei-corr") : null;
+
 const corresp = props.node
   ? props.node.getAttribute("corresp")?.replace("#", "")
   : null;
@@ -31,9 +34,29 @@ if (corresp) {
           <TeiNodes :nodes="orig.childNodes" />
         </span>
       </template>
-      <v-card width="500">
+      <v-card max-width="500">
         <div class="pa-4">
           <TeiNodes :nodes="reg.childNodes" />
+        </div>
+        <template v-if="note">
+          <v-divider></v-divider>
+          <div class="pa-4">
+            <TeiNodes :nodes="note.childNodes" />
+          </div>
+        </template>
+      </v-card>
+    </v-menu>
+  </template>
+  <template v-else-if="sic && corr">
+    <v-menu offset-y>
+      <template v-slot:activator="{ props }">
+        <span v-bind="props" class="clickable text-error">
+          <TeiNodes :nodes="sic.childNodes" />
+        </span>
+      </template>
+      <v-card max-width="500">
+        <div class="pa-4">
+          <TeiNodes :nodes="corr.childNodes" />
         </div>
         <template v-if="note">
           <v-divider></v-divider>

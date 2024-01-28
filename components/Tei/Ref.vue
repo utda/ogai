42fb5245e51ebd90ddad788a9857447efc5dd866
Satoru <you@example.com>
2024-01-29 06:58:06 +0900

    feat: add ref

---
+++ components/Tei/Ref.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+interface PropType {
+  node: Element | null;
+}
+
+const props = withDefaults(defineProps<PropType>(), {
+  node: null,
+});
+
+const url = props.node
+  ? props.node.getAttribute("target")?.replace("#", "")
+  : null;
+</script>
+<template>
+  <template v-if="url">
+    <a :href="url">
+      <TeiNodes :nodes="node?.childNodes" />
+    </a>
+  </template>
+  <template v-else>
+    <TeiNodes :nodes="node?.childNodes" />
+  </template>
+</template>
